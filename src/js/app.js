c669df4c8c2c0e54a1b494a9c34c4379e0934b30
Ericka Castillo <84164725+erickact@users.noreply.github.com>
2024-08-02 17:11:33 +0000

    conditional profile card

--- src/js/app.js
+++ src/js/app.js
@@ -29,18 +29,35 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  let name = `${variables.name ? variables.name : "Ericka"}`;
+  let lastName = `${variables.lastName ? variables.lastName : "Castillo"}`;
+  let role = `${variables.role ? variables.role : "Web Developer"}`;
+  let city = `${variables.city ? variables.city : "Lima"}`;
+  let country = `${variables.country ? variables.country : "Peru"}`;
+  let mediaPosition = `${
+    variables.socialMediaPosition
+      ? variables.socialMediaPosition
+      : "position-right"
+  }`;
+  let twitter = `${variables.twitter ? variables.twitter : "4geeksacademy"}`;
+  let github = `${variables.github ? variables.github : "4geeksacademy"}`;
+  let linkedin = `${variables.linkedin ? variables.linkedin : "4geeksacademy"}`;
+  let instagram = `${
+    variables.instagram ? variables.instagram : "4geeksacademy"
+  }`;
+
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${name} ${lastName}</h1>
+          <h2>${role}</h2>
+          <h3>${city} ${country}</h3>
+          <ul class=${mediaPosition}>
+            <li><a href="https://twitter.com/${twitter}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${github}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/school/${linkedin}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -54,7 +71,8 @@ window.onload = function() {
     // if includeCover is true the algorithm should show the cover image
     includeCover: true,
     // this is the image's url that will be used as a background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
     // social media bar position (left or right)
